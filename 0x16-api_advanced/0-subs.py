66b89a8b6bb7d4c8c95a5c673c84518cd78c71eb
eyzee00 <azizihamza1910@gmail.com>
2024-04-12 01:46:09 +0000

    f

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and returns the number of subscribers for a given subreddit.
-If the subreddit is invalid, it returns 0.
+Module to interface with the reddit api
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'by u/qasqot79'}  # Set a custom User-Agent to avoid issues
-
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
+    """
+    Uses the reddit api to get the numbers of subscribers to a subreddit
+    """
+    url = 'https://reddit.com/r/' + subreddit + '/about/.json'
+    headers = {'User-Agent': "lala"}
+    r = requests.get(url, headers=headers)
+    try:
+        return(r.json().get('data').get('subscribers'))
+    except:
+        return(0)
