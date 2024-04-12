d6299af7fcab7bdad2d09f8a14a38bbdc367a83f
eyzee00 <azizihamza1910@gmail.com>
2024-04-12 01:22:41 +0000

    f

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Google Chrome Version 81.0.4044.129"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
